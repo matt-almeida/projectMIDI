7107dbca066dc8f2268b1cc35cfd6c3468c0887a
Matheus <matheusalmeidad20@gmail.com>
2025-02-03 17:57:17 -0300

    Changing the while loop to for in the buttons/audio configuration

--- main.js
+++ main.js
@@ -6,20 +6,14 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let cont = 0;
 
-while (cont < listaDeTeclas.length) {
+for (let cont = 0; cont < listaDeTeclas.length; cont++) {
 
     const tecla = listaDeTeclas[cont];
     const instrumento = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumento}`;
-    
-    //console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
     
-    cont = cont + 1;
-
-    //console.log(cont);
 }
