8c53606270871467bb880e5b79ed1db463961fb0
Matheus <matheusalmeidad20@gmail.com>
2025-02-10 21:44:37 -0300

    Final implementation with optimized .main

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } 
+    else {
+        console.log('Element não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -15,5 +22,17 @@ for (let cont = 0; cont < listaDeTeclas.length; cont++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
